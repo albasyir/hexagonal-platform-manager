15be47ee2eb96c2a992aacf5a50664e8cb55de60
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2025-05-28 06:12:34 +0700

    refactor: remove export of HttpPlatformResponse to streamline type definitions

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,4 @@
 export * from './http-platform-method';
 export * from './http-platform-request';
-export * from './http-platform-response'; 
 export * from './http-platform-router';
 export * from './http-platform';
